ecc17ea8a208e3c90c1de534bfce4f7938fa15ad
Ren De <jer12182002@gmail.com>
2020-09-21 16:24:40 -0400

    finished add item input field, ready to build server for the api

--- src/components/loggedInUser/inventory/addItem/addItem.js
+++ src/components/loggedInUser/inventory/addItem/addItem.js
@@ -3,7 +3,7 @@ import './addItem.scss';
 
 import {connect} from 'react-redux';
 
-import {LOAD_ITEM_TYPE } from 'redux/actions/addItemAction';
+import {LOAD_ITEM_TYPE, ITEM_TYPE_IN, ADD_BTN_CLICKED, inputValue } from 'redux/actions/addItemAction';
 
 
 class addItem extends React.Component{
@@ -11,11 +11,12 @@ class addItem extends React.Component{
 	componentDidMount() {
     	const {LOAD_ITEM_TYPE } = this.props;
 		LOAD_ITEM_TYPE();
+		console.log(inputValue);
 	}
 
 
     render() {
-    
+    	
         return (
             <div className="addItem-wrapper container-fluid">
             	<div className="row">zxc</div>
@@ -24,15 +25,16 @@ class addItem extends React.Component{
             		<div className="input-container col-11">
             			<div>
 	            			<p>Name</p>
-	            			<input type="text"/>
+	            			<input id="ADDITEM_ENGLISH_NAME" type="text" onChange={e=> ITEM_TYPE_IN('ENGLISH_NAME', e.target.value)}/>
             			</div>
             			<div>
             				<p>名稱</p>
-            				<input type="text"/>
+            				<input id="ADDITEM_CHINESE_NAME" type="text" onChange={e=> ITEM_TYPE_IN('CHINESE_NAME', e.target.value)}/>
             			</div>
             			<div>
             				<p>Type</p>
-            				<select>
+            				<select id="ADDITEM_TYPE" onLoad = {e=> ITEM_TYPE_IN('TYPE', e.target.value)} onChange={e=> ITEM_TYPE_IN('TYPE', e.target.value)}>
+            						<option></option>
             					{this.props.itemTypes.map(type=>
             						<option key= {type.ID}>{type.ITEM_TYPE}</option>
             					)}
@@ -40,24 +42,24 @@ class addItem extends React.Component{
             			</div>
             			<div>
             				<p>QTY</p>
-            				<input type="number" min="0"/>
+            				<input type="number" min="0" onChange={e=> ITEM_TYPE_IN('QTY', e.target.value)}/>
             			</div>
             			<div>
             				<p>Studen $</p>
-            				<input type="number" min="0" step="0.01"/>
+            				<input type="number" min="0" step="0.01" onChange={e=> ITEM_TYPE_IN('RENDE_PRICE', e.target.value)}/>
             			</div>
             			<div>
             				<p>Professor $</p>
-            				<input type="number" min="0" step="0.01"/>
+            				<input type="number" min="0" step="0.01" onChange={e=> ITEM_TYPE_IN('STUDENT_PRICE', e.target)}/>
             			</div>
             			<div>
             				<p>Ren De $</p>
-            				<input type="number" min="0" step="0.01"/>
+            				<input type="number" min="0" step="0.01" onChange={e=> ITEM_TYPE_IN('PROFESSOR_PRICE', e.target.value)}/>
             			</div>
             		</div>
             		<div className="button-container col-1">
             			<div>
-            				<button type="button" className="btn btn-success" onClick={LOAD_ITEM_TYPE}>Add</button>
+            				<button type="button" className="btn btn-success" onClick={e => {e.preventDefault();ADD_BTN_CLICKED();}}>Add</button>
             			</div>
             		</div>
             	</div>
--- src/redux/actions/addItemAction.js
+++ src/redux/actions/addItemAction.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-let inputValue = {
+export let inputValue = {
 	ENGLISH_NAME : "",
 	CHINESE_NAME : "",
-	TPYE : "",
+	TYPE : "",
 	QTY : "",
 	RENDE_PRICE : 0,
 	STUDENT_PRICE : 0, 
@@ -32,4 +32,53 @@ export const LOAD_ITEM_TYPE = (dispatch) => {
 };
 
 
+export const ITEM_TYPE_IN = (target, value) => {
+
+  switch (target) {
+    case 'ENGLISH_NAME':
+      inputValue.ENGLISH_NAME = value.trim().toLowerCase().replace(/\s\s/g, '').replace(/[^a-zA-Z0-9-]+(.)/g, (m, chr) => ' '+ chr.toUpperCase()).replace(/^[a-z]/g,c => c.toUpperCase());
+    break;
+
+    case 'CHINESE_NAME':
+      inputValue.CHINESE_NAME = value.trim().split("").filter(char => /\p{Script=Han}/u.test(char)).join("");
+    break;
+
+    case 'TYPE':
+      inputValue.TYPE = value;
+    break;
+
+    case 'QTY':
+      inputValue.QTY = value;
+    break;
+
+    case 'RENDE_PRICE':
+      inputValue.RENDE_PRICE = value;
+    break;
+
+    case 'STUDENT_PRICE':
+      inputValue.STUDENT_PRICE = value;
+    break;
+
+    case 'PROFESSOR_PRICE':
+      inputValue.PROFESSOR_PRICE = value;
+    break;
+  }
+  console.log(inputValue);
+}
+
+export const ADD_BTN_CLICKED = () => {
+  if(inputValue.ENGLISH_NAME.trim() && 
+     inputValue.CHINESE_NAME.trim() &&
+     inputValue.TYPE.trim() &&
+     inputValue.QTY &&
+     inputValue.RENDE_PRICE &&
+     inputValue.STUDENT_PRICE &&
+     inputValue.PROFESSOR_PRICE
+     ) {
+
+
+  }else {
+    alert('Input Error !!!, please check if your input is valid');
+  }
+}
 
